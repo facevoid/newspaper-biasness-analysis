6e6c5c9bb59f7e5752d63f510fd83d379d1422e1
root <root@localhost.localdomain>
2020-03-10 22:19:07 +0600

    south asian lang word2vec, fasttext

---
+++ word_sets_south_asian_languages_stanford_standards.py
@@ -0,0 +1,126 @@
+MALE_WORDS_ENGLISH = [ 'he', 'son', 'his', 'him', 'father', 'man', 'boy', 'himself', 'male', 'brother', 'sons', 'fathers', 'men', 'boys', 'males', 'brothers', 'uncle',
+'uncles', 'nephew', 'nephews']
+
+FEMALE_WORDS_ENGLISH = ['she', 'daughter', 'hers', 'her', 'mother', 'woman', 'girl', 'herself', 'female', 'sister', 'daughters', 'mothers', 'women',
+'girls', 'femen', 'sisters', 'aunt', 'aunts', 'niece', 'nieces']
+
+PROFESSIONAL_OCCUPATIONS_ENGLISH = ['janitor', 'statistician', 'midwife', 'bailiff', 'auctioneer', 'photographer', 'geologist', 'shoemaker', 'athlete', 'cashier',
+'dancer', 'housekeeper', 'accountant', 'physicist', 'gardener', 'dentist', 'weaver', 'blacksmith', 'psychologist', 'supervisor',
+'mathematician', 'surveyor', 'tailor', 'designer', 'economist', 'mechanic', 'laborer', 'postmaster', 'broker', 'chemist', 'librarian', 'attendant', 'clerical', 'musician', 'porter', 'scientist', 'carpenter', 'sailor', 'instructor', 'sheriff', 'pilot', 'inspector', 'mason',
+'baker', 'administrator', 'architect', 'collector', 'operator', 'surgeon', 'driver', 'painter', 'conductor', 'nurse', 'cook', 'engineer',
+'retired', 'sales', 'lawyer', 'clergy', 'physician', 'farmer', 'clerk', 'manager', 'guard', 'artist', 'smith', 'official', 'police', 'doctor',
+'professor', 'student', 'judge', 'teacher', 'author', 'secretary', 'soldier']
+
+MALE_WORDS_BENGALI = ['তিনি', 'পুত্র', 'তার', 'তাঁকে', 'পিতা', 'মানুষ', 'ছেলে', 'নিজে', 'পুরুষ', 'ভাই', 'পুত্র', 'পিতৃ', ' পুরুষ ',' ছেলে ',' পুরুষ ',' ভাই ',' চাচা ',
+'চাচা', 'ভাতিজা', 'ভাগ্নে']
+
+MALE_WORDS_HINDI = ['वह ', 'बेटा', 'उसका ', 'उसे', 'पिता ',' आदमी', 'लड़का ', 'पुरुष ',' भाई', 'बेटे ', 'वालिद', ' पुरुष ',' लड़के ',' पुरुष ',' भाई ',' चाचा ',
+'अंकल', 'भतीजे', 'भतीजे']
+
+MALE_WORDS_URDU = ['وہ' , 'بیٹا' , 'اس' , 'اس' , 'باپ' , 'آدمی' , 'لڑکے' , 'خود' , 'مرد' , 'بھائی' , 'بیٹے' , 'باپ' , ' مرد ',' لڑکے ',' نر ',' بھائی ',' چچا ',
+'ماموں' , 'بھتیجے' , 'بھتیجے']
+
+MALE_WORDS_NEPALI = ['ऊ', 'छोरा', 'उसको', 'उसलाई', 'पिता', 'मान्छे', 'केटा', 'आफै', 'पुरुष', 'भाई', 'छोरा', 'बुबाहरू', ' पुरुष ',' केटाहरू ',' पुरुषहरू ',' भाइहरू ',' अंकल ',
+'काका', 'भतिजा', 'भतिजा']
+
+FEMALE_WORDS_BENGALI = ['সে', 'কন্যা', 'তাঁর', 'তার', 'মা', 'মহিলা', 'মেয়ে', 'নিজে', 'মহিলা', 'বোন', 'কন্যা', 'মা', ' নারী ',
+'মেয়েরা', 'মেয়ে', 'বোন', 'খালা', 'মাসি', 'ভাতিজি', 'ভাতিজি']
+
+FEMALE_WORDS_HINDI = ['वह', 'बेटी', 'उसकी', 'उसकी', 'मां', 'महिला', 'लड़की', 'खुद', 'महिला', 'बहन', 'बेटियां', 'मां', ' महिलाओं',
+'लड़कियाँ', 'स्त्री', 'बहनें', 'चाची', 'चाची', 'भतीजी', 'भतीजी']
+
+FEMALE_WORDS_URDU = ['وہ' , 'بیٹی' , 'اس' , 'اس' , 'ماں' , 'عورت' , 'لڑکی' , 'خود' , 'خواتین' , 'بہنیں' , 'بیٹیاں' , 'ماؤں' , ' خواتین ',
+'لڑکیاں' , 'خواتین' , 'بہنیں' , 'خالہ' , 'خالہ' , 'بھانجی' , 'بھانجی']
+
+FEMALE_WORDS_NEPALI = ['उनी', 'छोरी', 'उनको', 'उनको', 'आमा', 'महिला', 'केटी', 'आफै', 'महिला', 'बहिनी', 'छोरीहरू', 'आमाहरू', ' महिला ',
+'केटीहरू', 'महिलाहरू', 'बहिनीहरू', 'काकी', 'काकी', 'भतिजी', 'भान्जीहरू']
+
+
+PROFESSIONAL_OCCUPATIONS_BENGALI = ['দারোয়ান', 'পরিসংখ্যানবিদ', 'মিডওয়াইফ', 'বেলিফ', 'নিলাম', 'ফটোগ্রাফার', 'ভূতত্ত্ববিদ', 'জুতো প্রস্তুতকারক', 'অ্যাথলেট', 'ক্যাশিয়ার',
+'নর্তকী', 'গৃহকর্মী', 'হিসাবরক্ষক', 'পদার্থবিদ', 'উদ্যানবিদ', 'দাঁতের', 'তাঁতি', 'কামার', 'মনোবিজ্ঞানী', 'তত্ত্বাবধায়ক',
+'গণিতবিদ', 'সমীক্ষক', 'দর্জি', 'ডিজাইনার', 'অর্থনীতিবিদ', 'যান্ত্রিক', 'শ্রমিক', 'পোস্টমাস্টার', 'ব্রোকার', 'রসায়নবিদ', 'গ্রন্থাগারিক', 'পরিচারক', 'কেরানি ',' সুরকার ',' কুলি ',' বিজ্ঞানী ',' ছুতার ',' নাবিক ',' প্রশিক্ষক ',' শেরিফ ',' পাইলট ',' পরিদর্শক ',' রাজমিস্ত্রি ',
+'বেকার', 'প্রশাসক', 'স্থপতি', 'সংগ্রাহক', 'অপারেটর', 'সার্জন', 'চালক', 'চিত্রশিল্পী', 'কন্ডাক্টর', 'নার্স', 'কুক', 'ইঞ্জিনিয়ার',
+'অবসরপ্রাপ্ত', 'বিক্রয়', 'আইনজীবী', 'পাদ্রী', 'চিকিত্সক', 'কৃষক', 'কেরানি', 'পরিচালক', 'রক্ষী', 'শিল্পী', 'স্মিথ', 'অফিসিয়াল', 'পুলিশ ',' ডাক্তার ',
+'অধ্যাপক', 'ছাত্র', 'বিচারক', 'শিক্ষক', 'লেখক', 'সচিব', 'সৈনিক']
+
+PROFESSIONAL_OCCUPATIONS_HINDI = ['चौकीदार', 'सांख्यिकीविद्', 'दाई', 'बेलीफ', 'नीलामकर्ता', 'फोटोग्राफर', 'भूविज्ञानी', 'शोमेकर', 'एथलीट', 'कैशियर',
+'डांसर', 'हाउसकीपर', 'अकाउंटेंट', 'फिजिसिस्ट', 'माली', 'डेंटिस्ट', 'जुलाहा', 'लोहार', 'मनोवैज्ञानिक', 'सुपरवाइजर', 
+'गणितज्ञ', 'सर्वेक्षक', 'दर्जी', 'डिजाइनर', 'अर्थशास्त्री', 'मैकेनिक', 'मजदूर', 'पोस्टमास्टर', 'दलाल', 'केमिस्ट', 'लाइब्रेरियन', 'परिचर', 'लिपिक' ,' संगीतकार ',' कुली ',' वैज्ञानिक ',' बढ़ई ',' नाविक ',' प्रशिक्षक ',' शेरिफ ',' पायलट ',' निरीक्षक ',' राजमिस्त्री ',
+'बेकर', 'एडमिनिस्ट्रेटर', 'आर्किटेक्ट', 'कलेक्टर', 'ऑपरेटर', 'सर्जन', 'ड्राइवर', 'पेंटर', 'कंडक्टर', 'नर्स', 'कुक', 'इंजीनियर',
+'सेवानिवृत्त', 'बिक्री', 'वकील', 'पादरी', 'चिकित्सक', 'किसान', 'क्लर्क', 'प्रबंधक', 'रक्षक', 'कलाकार', 'स्मिथ', 'अधिकारी', 'पुलिस' , 'चिकित्सक',
+'प्रोफेसर', 'छात्र', 'जज', 'शिक्षक', 'लेखक', 'सचिव', 'सैनिक']
+
+PROFESSIONAL_OCCUPATIONS_URDU = ['جینیٹر' , 'شماریات دان' , 'دایہ' , 'بیلف' , 'نیلامی' , 'فوٹوگرافر' , 'ماہر ارضیات' , 'جوتا بنانے والا' , 'ایتھلیٹ' , 'کیشئیر' ,
+'ڈانسر' , 'گھریلو ملازم' , 'اکاؤنٹنٹ' , 'طبیعیات دان' , 'مالی' , 'دانتوں کا ڈاکٹر' , 'ویور' , 'لوہار' , 'ماہر نفسیات' , 'سپروائزر' ,
+'ریاضی دان' , 'سرویئر' , 'درزی' , 'ڈیزائنر' , 'ماہر معاشیات' , 'مکینک' , 'مزدور' , 'پوسٹ ماسٹر' , 'بروکر' , 'کیمسٹ' , 'لائبریرین' , 'حاضر خدمت' , 'مولوی ',' موسیقار ',' پورٹر ',' سائنسدان ',' بڑھئی ',' نااخت ',' انسٹرکٹر ',' شیرف ',' پائلٹ ',' انسپکٹر ',' میسن ',
+'بیکر' , 'ایڈمنسٹریٹر' , 'معمار' , 'کلکٹر' , 'آپریٹر' , 'سرجن' , 'ڈرائیور' , 'پینٹر' , 'کنڈیکٹر' , 'نرس' , 'کک' , 'انجینئر' ,
+'ریٹائرڈ' , 'سیلز' , 'وکیل' , 'پادری' , 'فزیشن' , 'کسان' , 'کلرک' , 'منیجر' , 'گارڈ' , 'آرٹسٹ' , 'سمتھ' , 'آفیشل' , 'پولیس ',' ڈاکٹر ',
+'پروفیسر' , 'طالب علم' , 'جج' , 'اساتذہ' , 'مصنف' , 'سکریٹری' , 'سپاہی']
+
+PROFESSIONAL_OCCUPATIONS_NEPALI = ['चौकीदार', 'सांख्यिकीय', 'सुँडेनी', 'बेलिफ', 'लिलामी', 'फोटोग्राफर', 'भूविज्ञानी', 'जुत्ता निर्माता', 'खेलाडी', 'क्यासियर',
+'नर्तक', 'हाउसकीपर', 'एकाउन्टेन्ट', 'भौतिक विज्ञ', 'माली', 'दंत चिकित्सक', 'जुलाहा', 'लोहार', 'मनोवैज्ञानिक', 'सुपरवाइजर',
+'गणितज्ञ', 'सर्वेक्षणकर्ता', 'दर्जी', 'डिजाइनर', 'अर्थशास्त्री', 'मेकानिक', 'मजदूर', 'पोस्टमास्टर', 'दलाल', 'रसायन विज्ञ', 'लाइब्रेरियन', 'परिचर', 'लिपिक ',' संगीतकार ',' पोर्टर ',' वैज्ञानिक ',' बढ़ई ',' नाविक ',' प्रशिक्षक ',' शेरिफ ',' पायलट ',' इन्स्पेक्टर ',' मेसन ',
+'बेकर', 'प्रशासक', 'आर्किटेक्ट', 'कलेक्टर', 'अपरेटर', 'सर्जन', 'ड्राइभर', 'पेन्टर', 'कन्डक्टर', 'नर्स', 'कुक', 'इन्जिनियर',
+'सेवानिवृत्त', 'बिक्री', 'वकिल', 'पादरीहरू', 'चिकित्सक', 'किसान', 'क्लर्क', 'म्यानेजर', 'गार्ड', 'कलाकार', 'स्मिथ', 'अफिसियल', 'पुलिस ',' चिकित्सक ',
+'प्राध्यापक', 'विद्यार्थी', 'न्यायाधीश', 'शिक्षक', 'लेखक', 'सचिव', 'सिपाही']
+
+
+south_asian_language_word_list_for_fasttext = {
+    'bn': {
+        'male_words': MALE_WORDS_BENGALI,
+        'female_words': FEMALE_WORDS_BENGALI,
+        'pr_occupations': PROFESSIONAL_OCCUPATIONS_BENGALI
+    },
+
+    'hi': {
+        'male_words': MALE_WORDS_HINDI,
+        'female_words': FEMALE_WORDS_HINDI,
+        'pr_occupations': PROFESSIONAL_OCCUPATIONS_HINDI
+    },
+    'ne': {
+        'male_words': MALE_WORDS_NEPALI,
+        'female_words': FEMALE_WORDS_NEPALI,
+        'pr_occupations': PROFESSIONAL_OCCUPATIONS_NEPALI
+    },
+    'ur': {
+        'male_words': MALE_WORDS_URDU,
+        'female_words': FEMALE_WORDS_URDU,
+        'pr_occupations': PROFESSIONAL_OCCUPATIONS_URDU
+    },
+    'en': {
+        'male_words': MALE_WORDS_ENGLISH,
+        'female_words': FEMALE_WORDS_ENGLISH,
+        'pr_occupations': PROFESSIONAL_OCCUPATIONS_ENGLISH
+    }
+    
+}
+
+south_asian_language_word_list_for_word2vec = {
+    'top-five_newspaper_300_model': {
+        'male_words': MALE_WORDS_BENGALI,
+        'female_words': FEMALE_WORDS_BENGALI,
+        'pr_occupations': PROFESSIONAL_OCCUPATIONS_BENGALI
+    },
+
+    'hindi': {
+        'male_words': MALE_WORDS_HINDI,
+        'female_words': FEMALE_WORDS_HINDI,
+        'pr_occupations': PROFESSIONAL_OCCUPATIONS_HINDI
+    },
+    'nepali_embeddings_word2vec': {
+        'male_words': MALE_WORDS_NEPALI,
+        'female_words': FEMALE_WORDS_NEPALI,
+        'pr_occupations': PROFESSIONAL_OCCUPATIONS_NEPALI
+    },
+    'urduvec_140M_100K_300d': {
+        'male_words': MALE_WORDS_URDU,
+        'female_words': FEMALE_WORDS_URDU,
+        'pr_occupations': PROFESSIONAL_OCCUPATIONS_URDU
+    },
+    'en': {
+        'male_words': MALE_WORDS_ENGLISH,
+        'female_words': FEMALE_WORDS_ENGLISH,
+        'pr_occupations': PROFESSIONAL_OCCUPATIONS_ENGLISH
+    }
+    
+}
